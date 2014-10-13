6dc85f313a40e7cfc216e705e58112b9632688e0
Juan Ignacio Salas <jsalas@laterous.com>
2014-10-13 17:20:03 -0300

    Gesture recognition.

--- InputMobi.h
+++ InputMobi.h
@@ -6,6 +6,14 @@
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
 
+typedef enum : NSUInteger {
+    InputMobiGestureNone        = 0,
+    InputMobiGestureDoubleTap = (1 << 3),
+    InputMobiGestureTripleTap = (1 << 4),
+    InputMobiGestureLongPress = (1 << 5),
+    InputMobiGestureShake = (1 << 6),
+} InputMobiGestureMask;
+
 @interface InputMobi : NSObject
 {
     NSString *applicationKey;
@@ -16,6 +24,6 @@
 @property (nonatomic, retain) NSString *applicationKey;
 @property (nonatomic, retain) NSString *bundleId;
 
-+(void)initWithApplicationKey:(NSString*)_key;
++(void)initWithApplicationKey:(NSString*)_key andGesture:(InputMobiGestureMask)_gesture;
 
 @end
